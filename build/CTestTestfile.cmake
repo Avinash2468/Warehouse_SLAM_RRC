457c647ab69088e5a9bb8bbdc2e399ee8708bd5b
avinash <avinashprabhu2001@gmail.com>
2020-06-11 19:48:05 +0530

    Robot built successfully and commands working

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/avinash/ros_slam_meher/src
+# Build directory: /home/avinash/ros_slam_meher/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mybot_simple_control")
+subdirs("robot_description")
+subdirs("teleop_twist_keyboard")
